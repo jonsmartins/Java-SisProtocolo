cbb1c2a800ace986146c9f8abb8e862b2c4d14d4
Jonas Martins <37028535+jonsmartins@users.noreply.github.com>
2019-05-21 19:44:36 -0300

    Add files via upload
    adicionando txt de usuarios e classe que me ajudara na verificacao

---
+++ Usuarios.java
@@ -0,0 +1,18 @@
+public class Usuarios{
+    String usuario;
+    String senha;
+    Usuarios(String usuario, String senha){
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+    Usuarios(){
+
+    }
+    public String getNome(){
+        return this.usuario;
+    }
+    public String getSenha(){
+        return this.senha;
+    }
+
+}
