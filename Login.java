953ec40836e858964cd12ecda12542ea514a88b1
Jonas Martins <jonas_silva_rio@hotmail.com>
2019-05-22 00:00:27 -0300

    adicionando arraylist de usuarios e validacao de login

--- Login.java
+++ Login.java
@@ -1,16 +1,43 @@
 package br.com.uva;
 
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.swing.*;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Login {
 	public static void main(String args[]) {
+		//carregando meu arraylist de usuarios
+        ArrayList<Usuarios> usuarios = new ArrayList<Usuarios>();
+        try{
+		    File bduser = new File("user.txt");
+		    BufferedReader bufferedReader = new BufferedReader(new FileReader(bduser));
+		    String line;
+ 		    while((line = bufferedReader.readLine()) != null) {
+                String usuario = line.split(":")[0];
+                String senha = line.split(":")[1];
+        	    Usuarios us = new Usuarios(usuario, senha);
+        	    usuarios.add(us);
+            }
+            bufferedReader.close();  
+        }catch(IOException e){
+            System.out.println("Ocorreu um erro");
+            e.printStackTrace();
+        }
+
 		// começa a preparar a janela Home
-		JFrame frame = new JFrame("HOME");
+		JFrame frame = new JFrame("Protocolos");
 
 		// Cria label e textField do Login
 		JLabel labelLogin = new JLabel("Login");
@@ -25,15 +52,46 @@ public class Login {
 		textFieldSenha.setBounds(25, 160, 170, 40);
 
 		//Logouva
-		ImageIcon imglogo = new ImageIcon("C:\\Projetos\\JavaProj\\src\\br\\com\\uva\\uvalogo.png");
+		ImageIcon imglogo = new ImageIcon("uvalogo.png");
 		JLabel logo = new JLabel(imglogo);
 		logo.setBounds(200, 40, 180, 100);
 		logo.setVisible(true);
 
-
 		// Cria botao entrar
 		JButton btnEntrar = new JButton("Entrar");
 		btnEntrar.setBounds(250, 160, 100, 50);;
+		btnEntrar.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//criando janela de erro
+				JFrame erroframe = new JFrame("Erro");
+				erroframe.setSize(290,170);
+				erroframe.setLayout(null);
+				erroframe.setVisible(true);
+				JLabel errolb = new JLabel("Login ou senha invalidos");	
+				errolb.setBounds(50, 25, 200, 70);
+				JButton btnErro = new JButton("Ok");
+				btnErro.setBounds(100, 80, 50, 30);
+				btnErro.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						erroframe.setVisible(false);
+					}
+				});
+				//validando informacoes
+				for(int i=0; i<usuarios.size();i++){
+					if(usuarios.get(i).getNome()==textFieldLogin.getText()){
+						if(usuarios.get(i).getSenha()==textFieldSenha.getText()){
+							Painel painel = new Painel();
+							//painel.montaTela();
+						}else{
+							erroframe.add(errolb);
+							erroframe.add(btnErro);}
+					}else{
+						erroframe.add(errolb);
+						erroframe.add(btnErro);}
+							
+				}
+			}
+		});
 
 		// Adiciona components para a tela
 		frame.add(labelLogin);
